b3591271822d1f87d958a24ce46a3ef016e55b73
vincent.baer <vincent.baer@outscale.com>
2023-02-28 20:51:41 +0000

    hello

--- test.py
+++ test.py
@@ -5,7 +5,7 @@ remote = f"https://{username}:{password}@github.com/outscale-vbr/upjet-provider-
 repo = Repo()
 repo.git.add("*")
 repo.index.commit("hello")
-Repo.clone_from(remote,)
+Repo.clone_from(remote,".")
 repo.remote()
 origin = repo.remote(name="origin")
 origin.push()
