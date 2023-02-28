d87430f8f3679c671b401975fd1eeb51bff6764f
vincent.baer <vincent.baer@outscale.com>
2023-02-28 20:55:11 +0000

    hello

--- test.py
+++ test.py
@@ -1,11 +1,12 @@
 from git import Repo
 username = "outscale-vbr"
+full_local_path = "/home/outscale/upjet-provider-outscale"
 password = "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"
 remote = f"https://{username}:{password}@github.com/outscale-vbr/upjet-provider-outscale.git"
-repo = Repo()
+repo = Repo(full_local_path)
 repo.git.add("*")
 repo.index.commit("hello")
-Repo.clone_from(remote,".")
+Repo.clone_from(remote,full_local_path)
 repo.remote()
 origin = repo.remote(name="origin")
 origin.push()
